c133aa5cc6b8bfd6e7514f7cf77716de9a020ea2
berFiras <firasberrajah94@gmail.com>
2025-03-28 11:09:59 +0100

    Add: members stories

---
+++ src/utils/StoriesMock.ts
@@ -0,0 +1,34 @@
+import StoriesMockType from "../types/types/StoriyType.type";
+
+const StoriesMock: StoriesMockType[] = [
+  {
+    id: 1,
+    title: "John’s Story",
+    description:
+      "Non arcu risus quis varius quam quisque. Ornare suspendisse sed nisi lacus. Eu feugiat pretium nibh ",
+    imageUrl: "src/assets/images/story1.jpeg",
+  },
+  {
+    id: 2,
+    title: "The Journey",
+    description:
+      "Proin nibh nisl condimentum id venenatis a condimentum vitae sapien. Tellus in metus vulputate eu sc",
+    imageUrl: "src/assets/images/story2.jpeg",
+  },
+  {
+    id: 3,
+    title: "Catch Day",
+    description:
+      "Elit pellentesque habitant morbi tristique senectus et netus et. In hendrerit gravida rutrum quisque",
+    imageUrl: "src/assets/images/story3.jpeg",
+  },
+  {
+    id: 4,
+    title: "Trout tails",
+    description:
+      "Sit amet mattis vulputate enim nulla aliquet. At augue eget arcu dictum varius. Volutpat commodo sed",
+    imageUrl: "src/assets/images/story4.jpeg",
+  },
+];
+
+export default StoriesMock;
